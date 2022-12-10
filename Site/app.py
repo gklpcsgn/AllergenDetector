3f9353777ad7bcb3a0e63008d2d8055cc28d746f
Gökalp Coşgun <gklpcsgn94@gmail.com>
2022-12-11 01:18:59 +0300

    First Connection

--- Site/app.py
+++ Site/app.py
@@ -4,9 +4,26 @@ import pickle
 import socket
 
 app = Flask(__name__)
+# set the secret key.  keep this really secret:
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((socket.gethostname(), 1234))
+        print('Connected to server.')
+    except Exception as e:
+        flash('Connection Error.', category='error')
+        return render_template('index.html')
+
+    message = "Gökalpten selamlar"
+    message = message.encode('utf-8')
+    client.send(message)
+    from_server = client.recv(4096)
+    from_server = from_server.decode('utf-8')
+    print("From server : ",from_server)
+    client.close()
     return render_template('index.html')
 
 @app.route("/search", methods=['POST'])
--- server.py
+++ server.py
@@ -2,17 +2,44 @@ import pandas as pd
 import pandasql as ps
 import sqlalchemy as sal
 from sqlalchemy import create_engine
+import socket
 
-# connect to local host
-engine = create_engine('postgresql://postgres:146146@localhost/Proje')
+# # connect to local host
+# try:
+#     engine = create_engine('postgresql://postgres:146146@localhost/Proje')
+#     print('Connected to database.')
+# except Exception as e:
+#     print('Connection Error.')
 
-# select from allergen table
-allergen = pd.read_sql_query('select * from allergen',con=engine)
+# # select from allergen table
+# allergen = pd.read_sql_query('select * from allergen',con=engine)
 
-print(allergen)
+# print(allergen)
 
-# close connection
-engine.dispose()
+
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv.bind(("", 1234))
+print("listening")
+serv.listen(5)
+while True:
+    conn, addr = serv.accept()
+    from_client = ''
+    while True:
+        data_user = conn.recv(4096)
+        if not data_user: break
+        data_user = data_user.decode('utf-8')
+        from_client += data_user
+        print ("From client : ",from_client)
+        print("To client : ","Hello from server")
+        message = "Hello from server"
+        message = message.encode('utf-8')
+        conn.send(message)
+    conn.close()
+    print ('client disconnected')
+    
+
+
+# engine.dispose()
 
 
 
