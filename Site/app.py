ed59dd05a0afa2bbe89315777d45f7bc42c02a85
İsmail Buğra Öz <ozismailbugra@gmail.com>
2022-12-11 01:54:21 +0300

    login ekranı ve hata toastu eklendi

--- Site/app.py
+++ Site/app.py
@@ -32,5 +32,9 @@ def search():
         search = request.form['search']
         return render_template('test.html', test=search)
 
+@app.route("/signin")
+def signin():
+    return render_template('signin.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
