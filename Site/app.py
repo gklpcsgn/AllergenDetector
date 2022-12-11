260d11c273fc08a7ad7783b22aabeb96bb77cad9
İsmail Buğra Öz <ozismailbugra@gmail.com>
2022-12-11 04:57:04 +0300

    düzgün arama ekranımsı birşey implement edildi

--- Site/app.py
+++ Site/app.py
@@ -60,6 +60,7 @@ def search():
 
 @app.route("/searchName", methods=['POST'])
 def searchName():
+    global isDebug
     if not isDebug:
         global client
         if request.method == 'POST':
@@ -86,9 +87,9 @@ def searchName():
             data = pd.read_json(from_server)
         return render_template('result.html', barcodeno=data['barcodeno'][0], foodname=data['foodname'][0], brand=data['brand'][0], weightvolume=data['weightvolume'][0], ingredients=data['ingredients'][0], fat=data['fat'][0], protein=data['protein'][0], carbs=data['carbs'][0], calorie=data['calorie'][0], allergennames=data['allergennames'][0])
     else:
-        from_server = "[{\"barcodeno\":1,\"foodname\":\"ekmek\",\"brand\":\"firinci\",\"weightvolume\":200,\"ingredients\":\"un\",\"fat\":20,\"protein\":10,\"carbs\":75,\"calorie\":300,\"allergennames\":\"['gluten', 'findik']\"}]"
+        from_server = '[{\"barcodeno\":1,\"brand\":\"firinci\",\"foodname\":\"ekmek\"},{\"barcodeno\":2,\"brand\":\"Eti\",\"foodname\":\"kek\"}]'
         data = pd.read_json(from_server)
-        return render_template('result.html', barcodeno=data['barcodeno'][0], foodname=data['foodname'][0], brand=data['brand'][0], weightvolume=data['weightvolume'][0], ingredients=data['ingredients'][0], fat=data['fat'][0], protein=data['protein'][0], carbs=data['carbs'][0], calorie=data['calorie'][0], allergennames=data['allergennames'][0])
+        return render_template('search_results.html', data=data)
 
 @app.route("/name_search")
 def searchbyname():
@@ -98,5 +99,39 @@ def searchbyname():
 def signin():
     return render_template('signin.html')
 
+@app.route("/item/<int:barcodeno>")
+def item(barcodeno):
+    if not isDebug:
+        global client
+        if client is None:
+            flash('Connection Error.', category='error')
+            print("Cannot connect to server.")
+            return render_template("index.html")
+        barkod = barcodeno
+        if not barkod.isdigit():
+            flash('Barkod yalnızca sayı içerebilir.', category='error') 
+            return render_template("index.html")
+        message = "b"
+        message += barkod
+        message = message.encode('utf-8')
+        client.send(message)
+        
+        # TODO : add ERROR handling
+        from_server = client.recv(4096)
+        from_server = from_server.decode('utf-8')
+        print("From server : ",from_server)
+
+        if from_server == "ERROR":
+            flash('Barkod bulunamadı.', category='error')
+            return render_template("index.html")
+
+        # return render_template('test.html', test=from_server)
+        data = pd.read_json(from_server)
+        return render_template('result.html', barcodeno=data['barcodeno'][0], foodname=data['foodname'][0], brand=data['brand'][0], weightvolume=data['weightvolume'][0], ingredients=data['ingredients'][0], fat=data['fat'][0], protein=data['protein'][0], carbs=data['carbs'][0], calorie=data['calorie'][0], allergennames=data['allergennames'][0])
+    else:
+        from_server = "[{\"barcodeno\":1,\"foodname\":\"ekmek\",\"brand\":\"firinci\",\"weightvolume\":200,\"ingredients\":\"un\",\"fat\":20,\"protein\":10,\"carbs\":75,\"calorie\":300,\"allergennames\":\"['gluten', 'findik']\"}]"
+        data = pd.read_json(from_server)
+        return render_template('result.html', barcodeno=data['barcodeno'][0], foodname=data['foodname'][0], brand=data['brand'][0], weightvolume=data['weightvolume'][0], ingredients=data['ingredients'][0], fat=data['fat'][0], protein=data['protein'][0], carbs=data['carbs'][0], calorie=data['calorie'][0], allergennames=data['allergennames'][0])
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=8080)
