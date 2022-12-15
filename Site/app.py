ba30b058c238fa51ec1458f8641689e0c667465d
Gökalp Coşgun <gklpcsgn94@gmail.com>
2022-12-15 04:35:04 +0300

    hotfix

--- Site/app.py
+++ Site/app.py
@@ -113,42 +113,35 @@ def home():
 
 @app.route("/search", methods=['POST'])
 def search():
-    if not isDebug:
-        global client
-        if request.method == 'POST':
-            if client is None:
-                flash('Connection Error.', category='error')
-                print("Cannot connect to server.")
-                return render_template("index.html")
-            barkod = request.form['search']
-            if not barkod.isdigit():
-                flash('Barkod yalnızca sayı içerebilir.', category='error') 
-                return render_template("index.html")
-            message = "b"
-            message += str(barkod)
-            message = message.encode('utf-8')
-            client.send(message)
-            
-            # TODO : add ERROR handling
-            from_server = client.recv(4096)
-            from_server = from_server.decode('utf-8')
-            print("From server : ",from_server)
+    global client
+    if request.method == 'POST':
+        if client is None:
+            flash('Connection Error.', category='error')
+            print("Cannot connect to server.")
+            return render_template("index.html")
+        barkod = request.form['search']
+        if not barkod.isdigit():
+            flash('Barkod yalnızca sayı içerebilir.', category='error') 
+            return render_template("index.html")
+        message = "b"
+        message += str(barkod)
+        message = message.encode('utf-8')
+        client.send(message)
+        
+        # TODO : add ERROR handling
+        from_server = client.recv(4096)
+        from_server = from_server.decode('utf-8')
+        print("From server : ",from_server)
 
-            if from_server == "ERROR":
-                flash('Barkod bulunamadı.', category='error')
-                return render_template("index.html")
+        if from_server == "ERROR":
+            flash('Barkod bulunamadı.', category='error')
+            return render_template("index.html")
 
-            # return render_template('test.html', test=from_server)
-            data = pd.read_json(from_server)
-            allergens = data["allergennames"][0].replace("'","\"")
-            allergens = json.loads(allergens)
-            print("allergens : ", allergens)
-            return render_template('result.html', barcodeno=data['barcodeno'][0], foodname=data['foodname'][0], brand=data['brand'][0], weightvolume=data['weightvolume'][0], ingredients=data['ingredients'][0], fat=data['fat'][0], protein=data['protein'][0], carbs=data['carbs'][0], calorie=data['calorie'][0], allergens=allergens)
-    else:
-        from_server = "[{\"barcodeno\":1,\"foodname\":\"ekmek\",\"brand\":\"firinci\",\"weightvolume\":200,\"ingredients\":\"un\",\"fat\":20,\"protein\":10,\"carbs\":75,\"calorie\":300,\"allergennames\":\"['gluten', 'findik']\"}]"
+        # return render_template('test.html', test=from_server)
         data = pd.read_json(from_server)
         allergens = data["allergennames"][0].replace("'","\"")
         allergens = json.loads(allergens)
+        print("allergens : ", allergens)
         return render_template('result.html', barcodeno=data['barcodeno'][0], foodname=data['foodname'][0], brand=data['brand'][0], weightvolume=data['weightvolume'][0], ingredients=data['ingredients'][0], fat=data['fat'][0], protein=data['protein'][0], carbs=data['carbs'][0], calorie=data['calorie'][0], allergens=allergens)
 
 @app.route("/searchName", methods=['POST'])
